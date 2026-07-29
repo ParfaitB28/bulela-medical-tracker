1a6b2182c022511f9edf5173f3eb8324b54a8fe7
Parfait Bulela <bulelaparfait@gmail.com>
2026-07-29 12:28:39 -0300

    Initialize Spring Boot project

---
+++ src/main/java/com/bulela/medicaltracker/BulelaMedicalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bulela.medicaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulelaMedicalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulelaMedicalTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bulela/medicaltracker/BulelaMedicalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulela.medicaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulelaMedicalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
